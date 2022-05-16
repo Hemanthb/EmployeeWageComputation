f3b3b19a98321cced492a10c33d57859495b284e
hem_b <hemanthb93@gmail.com>
2022-05-16 20:04:16 +0530

    [hemanth]:Attendance check of Employee

---
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeAttendance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeAttendance
+    {
+        Random random = new Random();
+        public void CheckAttendance()
+        {
+            if (random.Next(0, 2) == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
