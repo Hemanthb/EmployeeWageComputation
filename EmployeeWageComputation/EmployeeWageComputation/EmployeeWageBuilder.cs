1c86cc900bdd542e5893b15395849130dd5e9a2d
hem_b <hemanthb93@gmail.com>
2022-05-23 17:18:27 +0530

    [Hemanth]: Calculates wages for each company passed as objects

---
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeWageBuilder
+    {
+        const int IS_FULL_TIME = 1, IS_PART_TIME = 2;
+        private String company;
+        private int totalWorkingDays, maxWorkHours, wagePerHour;
+        Random random = new Random();
+        public EmployeeWageBuilder(String company,int totalWorkingDays,int maxWorkHours,int wagePerHour)
+        {
+            this.company = company;
+            this.totalWorkingDays = totalWorkingDays;
+            this.maxWorkHours = maxWorkHours;
+            this.wagePerHour = wagePerHour;
+
+        }
+        public void WageAsPerCompany()
+        {
+            int empWorkHours = 0, workingDays = 0;
+            while (empWorkHours <= maxWorkHours && workingDays <= totalWorkingDays)
+            {
+                workingDays++;
+                int workMode = random.Next(0, 3);
+
+                switch (workMode)
+                {
+                    case IS_FULL_TIME:
+                        empWorkHours += 8;
+                        break;
+                    case IS_PART_TIME:
+                        empWorkHours += 4;
+                        break;
+                    default:
+                        empWorkHours += 0;
+                        break;
+                }
+            }
+            int empWage = empWorkHours * wagePerHour;
+            Console.WriteLine("Employee wage of firm - {0} is Rs.{1} ", company, empWage);
+        }
+    }
+}
--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 EmployeeWageComputation.EmployeeDetails employeeAttendance = new EmployeeWageComputation.EmployeeDetails();
-employeeAttendance.CheckAttendance();
+EmployeeWageComputation.EmployeeWageBuilder wageBuilder = new EmployeeWageComputation.EmployeeWageBuilder("Lixil", 24, 160, 80);
+EmployeeWageComputation.EmployeeWageBuilder wageBuilder1 = new EmployeeWageComputation.EmployeeWageBuilder("Blabz", 25, 160, 100);
+wageBuilder.WageAsPerCompany();
+wageBuilder1.WageAsPerCompany();
+/*employeeAttendance.CheckAttendance();
 employeeAttendance.CalculateDailyWage();
 employeeAttendance.CalculatePartTimeWage();
 employeeAttendance.WageAsPerWorkHrs("XYZ&Co",25,200,50);
-employeeAttendance.WageAsPerWorkHrs("AbC Industries", 22, 160, 60);
+employeeAttendance.WageAsPerWorkHrs("AbC Industries", 22, 160, 60);*/
+
